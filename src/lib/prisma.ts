2af841bdba5d6b7cb99978bf96bdf7cd3066086d
rahul20805 <rahul20806@gmail.com>
2026-08-14 23:27:27 +0530

    fix: Support Vercel Postgres environment variables

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client"
 import { Pool } from "pg"
 import { PrismaPg } from "@prisma/adapter-pg"
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = `${process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL}`
 
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
